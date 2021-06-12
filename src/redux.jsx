6e6d30ed5bdb8aa89ae1e20059175fbdd51722b7
Nick <tydx11@hotmail.com>
2021-06-12 13:14:28 +0800

    change store api as real Redux store

--- src/redux.jsx
+++ src/redux.jsx
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 
 const isStateChange = (newState, oldState) => {
     for (const key in newState) {
@@ -8,46 +8,45 @@ const isStateChange = (newState, oldState) => {
     }
     return false
 }
+let _state = undefined;
+let _reducer = undefined;
+let _listeners = [];
 
+const setState = (newState) => {
+    _state = newState;
+    _listeners.map(fn => fn(_state));
+}
 
 export const store = {
-    state: {
-        user: {name: 'nick', age: 18},
-        group: 'frontend'
+    getState() {
+        return _state;
     },
-    setState(newState) {
-        store.state = newState;
-        store.listeners.map(fn => fn(store.state));
+    dispatch: (action) => {
+        setState(_reducer(_state, action))
     },
-    reducer: undefined,
-    listeners: [],
     subscribe: (fn) => {
-        store.listeners.push(fn)
+        _listeners.push(fn)
         return () => {
-            const index = store.listeners.findIndex(item => item === fn);
-            store.listeners.splice(index, 1);
+            const index = _listeners.findIndex(item => item === fn);
+            _listeners.splice(index, 1);
         }
     }
 }
 
-export const createStore = (reducer,initState) => {
-    store.reducer = reducer;
-    store.state = initState;
+export const createStore = (reducer, initState) => {
+    _reducer = reducer;
+    _state = initState;
     return store;
 }
 
 export const connect = (selector, mapDispatchToProps) => (Component) => {
     return (props) => {
-        const {state, setState} = useContext(appContext)
         const [, update] = useState(0);
-        const data = selector ? selector(state) : {state}
-        const dispatch = (action) => {
-            setState(store.reducer(state, action))
-        }
-        const dispatchers = mapDispatchToProps ? mapDispatchToProps(dispatch) : {dispatch}
+        const data = selector ? selector(_state) : {state: _state}
+        const dispatchers = mapDispatchToProps ? mapDispatchToProps(store.dispatch) : {dispatch: store.dispatch}
         useEffect(() => {
             return store.subscribe(() => {
-                const newState = store.state;
+                const newState = _state;
                 const newData = selector ? selector(newState) : {state: newState}
                 if (isStateChange(newData, data)) {
                     update(n => n + 1);
